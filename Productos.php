5217a16959b91b7f52a1f3dd33327fa892e21975
romordz <rickyromo32@hotmail.com>
2026-08-28 13:08:43 -0600

    Update stylesheet links in Productos.php to include SPrincipal.css and correct casing for SProductos.css

--- Productos.php
+++ Productos.php
@@ -10,7 +10,8 @@ $page_title = "Productos";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/Sproductos.css">
+    <!-- <link rel="stylesheet" href="css/SPrincipal.css"> -->
+    <link rel="stylesheet" href="css/SProductos.css">
     <title>productos</title>
 </head>
 
