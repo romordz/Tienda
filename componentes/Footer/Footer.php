42f5c4dc879051bf7afaa2a20b01d0501297793b
romordz <rickyromo32@hotmail.com>
2026-08-28 14:26:27 -0600

    Fix footer component inclusion and update stylesheet paths in Header and Footer components

--- componentes/Footer/Footer.php
+++ componentes/Footer/Footer.php
@@ -1,5 +1,5 @@
 <head>
-    <link rel="stylesheet" href="SFooter.css">
+    <link rel="stylesheet" href="/componentes/Header/SFooter.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 
