c348d722bb62b875641414e06a120af449c62f2d
romordz <rickyromo32@hotmail.com>
2026-08-28 00:24:38 -0600

    Update profile avatar handling to use base64 encoding and correct SQL query for avatar URL

--- php/Perfil_process.php
+++ php/Perfil_process.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['user_id'])) {
 $session_user_id = $_SESSION['user_id'];
 $profile_user_id = $_GET['id'] ?? $session_user_id;
 
-$sql = "SELECT nombre_usuario, nombre_completo, sexo, fecha_nacimiento, avatar, correo, rol, privacidad FROM usuarios WHERE id = ?";
+$sql = "SELECT nombre_usuario, nombre_completo, sexo, fecha_nacimiento, avatar_url, correo, rol, privacidad FROM usuarios WHERE id = ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$profile_user_id]);
 
@@ -20,7 +20,7 @@ if ($profile_data) {
     $profile_fullname = $profile_data['nombre_completo'];
     $profile_gender = $profile_data['sexo'];
     $profile_birthdate = $profile_data['fecha_nacimiento'];
-    $profile_avatar = base64_encode($profile_data['avatar']);
+    $profile_avatar = base64_encode($profile_data['avatar_url']);
     $profile_email = $profile_data['correo'];
     $profile_role = $profile_data['rol'];
     $profile_privacy = $profile_data['privacidad'];
