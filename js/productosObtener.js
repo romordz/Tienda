3287e404d8328a44d91b01befdf3b6e2722247d3
romordz <rickyromo32@hotmail.com>
2026-08-27 20:53:49 -0600

    Fix product item click handling and clean up registration photo validation

--- js/productosObtener.js
+++ js/productosObtener.js
@@ -29,8 +29,8 @@ function obtenerproductosDestacados() {
                         : `<span class="price">$${parseFloat(producto.precio).toFixed(2)}</span>`;
 
                     const productItem = `
-                        <div class="product-item">
-                            <a href="javascript:void(0);" onclick="checkSession('producto_detalle.php?id=${producto.id}');">
+                        <div class="product-item" onclick="checkSession('producto_detalle.php?id=${producto.id}');>
+                            <a href="javascript:void(0);"');">
                                 <img src="${imagenPrincipal}" alt="${producto.nombre}">
                             </a>
                             <h3>${producto.nombre}</h3>
@@ -77,8 +77,8 @@ function obtenerproductosRecientes() {
                         : `<span class="price">$${parseFloat(producto.precio).toFixed(2)}</span>`;
 
                     const productItem = `
-                        <div class="product-item">
-                            <a href="javascript:void(0);" onclick="checkSession('producto_detalle.php?id=${producto.id}');">
+                        <div class="product-item"  onclick="checkSession('producto_detalle.php?id=${producto.id}');>
+                            <a href="javascript:void(0);");">
                                 <img src="${imagenPrincipal}" alt="${producto.nombre}">
                             </a>
                             <h3>${producto.nombre}</h3>
