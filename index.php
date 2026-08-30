b5b3a7754ba28606063303a6d2701e0c2028fac3
romordz <rickyromo32@hotmail.com>
2026-08-29 23:55:58 -0600

    .

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
 require __DIR__ . '/php/config.php';
-
-if ($_SERVER['SCRIPT_NAME'] === '/index.php' && empty($_SERVER['PATH_INFO'])) {
-    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
-    $host = $_SERVER['HTTP_HOST'];
-    header("Location: " . $protocol . '://' . $host . '/');
-    exit;
-}
-
 require __DIR__ . '/pantallas/Principal.php';
 ?>
