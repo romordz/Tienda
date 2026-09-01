89f8d394dcc66b1b7e5f5459eddef10c18df218f
romordz <rickyromo32@hotmail.com>
2026-08-31 18:11:35 -0600

    fix: update autoload path in CloudinaryUploader.php

--- php/cloudinary/CloudinaryUploader.php
+++ php/cloudinary/CloudinaryUploader.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Cloudinary\Configuration\Configuration;
 use Cloudinary\Api\Upload\UploadApi;
